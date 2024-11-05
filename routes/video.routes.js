b1cf8e0cb0a093f37be2d48b6d594f0069fb35a4
venkatk-git <venkatkumar.profiles@gmail.com>
2024-11-05 20:45:52 +0530

    Added some video routes

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 const YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/videos";
 
-router.get("/gettitle", async (req, res) => {
+router.get("/getVideoTitle", async (req, res) => {
     const videoId = req.query.videoId;
 
     const url = `${YOUTUBE_API_URL}?id=${videoId}&key=${YOUTUBE_API_KEY}&part=snippet`;
@@ -30,6 +30,36 @@ router.get("/gettitle", async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: "Failed to get title" });
         console.error("Error fetching video title:", error);
+        throw error;
+    }
+});
+
+router.get("/getChannelInfo", async (req, res) => {
+    const videoId = req.query.videoId;
+
+    const url = `${YOUTUBE_API_URL}?id=${videoId}&key=${YOUTUBE_API_KEY}&part=snippet`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(
+                `Error fetching channel info: ${response.statusText}`
+            );
+        }
+        const data = await response.json();
+
+        if (data.items && data.items.length > 0) {
+            const snippet = data.items[0].snippet;
+            res.status(200).json({
+                channelName: snippet.channelTitle,
+                channelLogo: snippet.thumbnails.default.url, // Use "default", "medium", or "high" based on the desired size
+            });
+        } else {
+            res.status(404).json({ error: "Video not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get channel info" });
+        console.error("Error fetching channel info:", error);
         throw error;
     }
 });
